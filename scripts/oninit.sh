d67a7a3dd105a55d5a480ef1d41f567adf3b1aa3
ChillerDragon <ChillerDragon@gmail.com>
2021-07-04 21:18:38 +0200

    no proof

--- scripts/oninit.sh
+++ scripts/oninit.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-gdb -ex='set confirm off' \
-	-ex='set pagination off' \
-	-ex='set disassembly-flavor intel' \
-	-ex=run \
-	-ex=bt \
-	-ex=quit \
-	--args chillerbot-ux "$1"
+# yikes z-series client nobody saw that hrhrhrh
+
+chillerbot-zx "$1"
+
+# gdb -ex='set confirm off' \
+# 	-ex='set pagination off' \
+# 	-ex='set disassembly-flavor intel' \
+# 	-ex=run \
+# 	-ex=bt \
+# 	-ex=quit \
+# 	--args chillerbot-ux "$1"
 
