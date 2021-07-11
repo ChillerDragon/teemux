c3ff118a9f82cd4c758dd272f17078bf8e19c146
ChillerDragon <ChillerDragon@gmail.com>
2021-07-11 10:03:34 +0200

    I coded too many hax now i have crashbugs

--- scripts/oninit.sh
+++ scripts/oninit.sh
@@ -2,7 +2,12 @@
 
 # yikes z-series client nobody saw that hrhrhrh
 
-chillerbot-zx "$1"
+gdb -ex='set confirm off' \
+	-ex='set pagination off' \
+	-ex='set disassembly-flavor intel' \
+	-ex=run \
+	-ex=bt \
+	--args chillerbot-zx "$1"
 
 # gdb -ex='set confirm off' \
 # 	-ex='set pagination off' \
